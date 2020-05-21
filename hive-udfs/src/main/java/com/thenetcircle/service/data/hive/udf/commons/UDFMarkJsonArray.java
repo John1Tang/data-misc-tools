353e15f476be11d5f3524c9f78d8b7714da6038c
John1Tang <1253703152@qq.com>
2020-05-22 06:54:08 +0800

    test pass for udf "mark_json_array"

--- hive-udfs/src/main/java/com/thenetcircle/service/data/hive/udf/commons/UDFMarkJsonArray.java
+++ hive-udfs/src/main/java/com/thenetcircle/service/data/hive/udf/commons/UDFMarkJsonArray.java
@@ -1,21 +1,21 @@
 package com.thenetcircle.service.data.hive.udf.commons;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.session.SessionState;
-import org.apache.hadoop.hive.serde2.objectinspector.*;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.StringObjectInspector;
-import org.apache.hadoop.io.Text;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import static com.thenetcircle.service.data.hive.udf.UDFHelper.checkArgPrimitive;
-import static com.thenetcircle.service.data.hive.udf.UDFHelper.checkArgsSize;
+import static java.lang.String.format;
 import static org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory.javaStringObjectInspector;
 
 /**
@@ -23,25 +23,21 @@ import static org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveO
  * @author: john
  * @created: 2020/05/20 16:54
  */
-@Description(name = "json_array",
-        value = "_FUNC_(array_string) - Convert a string of a JSON-encoded array to a Hive array of strings.")
-public class UDFJsonAsArray extends UDTFExt {
+@Description(name = "mark_json_array",
+        value = "_FUNC_(array_string, delimiter) - Convert a string of a JSON-encoded array to a Hive array of strings.")
+public class UDFMarkJsonArray extends GenericUDF {
 
     public static final String NAME = "json_array";
 
     private transient StringObjectInspector strInsp;
 
-    public static final List<String> RESULT_FIELDS = Arrays.asList("json_obj");
-    public static final List<ObjectInspector> RESULT_FIELD_INSPECTORS = Arrays.asList(ObjectInspectorFactory.getStandardListObjectInspector(javaStringObjectInspector));
 
-    public static final StandardStructObjectInspector RESULT_TYPE = ObjectInspectorFactory.getStandardStructObjectInspector(
-            RESULT_FIELDS,
-            RESULT_FIELD_INSPECTORS);
+    public static final PrimitiveObjectInspector RESULT_TYPE = javaStringObjectInspector;
 
     @Override
-    public StructObjectInspector _initialize(ObjectInspector[] args) throws UDFArgumentException {
-        checkArgsSize(NAME, args, 1, 1);
-        checkArgPrimitive(NAME, args, 0);
+    public ObjectInspector initialize(ObjectInspector[] args) throws UDFArgumentException {
+        checkArgsSize(args, 2, 2);
+        checkArgPrimitive(args, 0);
 
         PrimitiveObjectInspector argOI;
         if (args[0] instanceof PrimitiveObjectInspector) {
@@ -68,18 +64,16 @@ public class UDFJsonAsArray extends UDTFExt {
     }
 
     @Override
-    public Object[] evaluate(Object[] _args, int start) throws HiveException {
-        if (_args == null) {
+    public Object evaluate(DeferredObject[] deferredObj) throws HiveException {
+        if (deferredObj[0].get() == null || deferredObj[1].get() ==null) {
             return null;
         }
         try {
-            JSONArray extractObject = new JSONArray(_args[start + 0].toString());
-            List<Text> result = new ArrayList<>();
-            for (int ii = 0; ii < extractObject.length(); ++ii) {
-                result.add(new Text(extractObject.get(ii).toString()));
-            }
-            //FIXME still cannot return as struct<array<string>>
-            return new Object[]{result};
+            JSONArray extractObject = new JSONArray(deferredObj[0].get().toString());
+            return IntStream
+                    .range(0,extractObject.length())
+                    .mapToObj(i -> extractObject.getJSONObject(i).toString())
+                    .collect(Collectors.joining(deferredObj[1].get().toString()));
         } catch (JSONException | NumberFormatException e) {
             e.printStackTrace(SessionState.getConsole().getChildErrStream());
             return new Object[]{-1, null, e.toString()};
@@ -87,8 +81,9 @@ public class UDFJsonAsArray extends UDTFExt {
     }
 
     @Override
-    public void close() throws HiveException {
-
+    public String getDisplayString(String[] children) {
+        return format("mark_json_array(%s)", StringUtils.join(children, ", "));
     }
+
 
 }
