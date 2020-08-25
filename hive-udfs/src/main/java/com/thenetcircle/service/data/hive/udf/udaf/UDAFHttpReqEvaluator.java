ffe348bda18b4c829b843a7900957ec129a7bd5b
john <john@thenetcircle.com>
2020-08-25 18:05:13 +0800

    to be fixed

--- hive-udfs/src/main/java/com/thenetcircle/service/data/hive/udf/udaf/UDAFHttpReqEvaluator.java
+++ hive-udfs/src/main/java/com/thenetcircle/service/data/hive/udf/udaf/UDAFHttpReqEvaluator.java
@@ -1,12 +1,18 @@
 package com.thenetcircle.service.data.hive.udf.udaf;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.thenetcircle.service.data.hive.udf.UDFHelper;
+import com.thenetcircle.service.data.hive.udf.commons.Jsons;
 import com.thenetcircle.service.data.hive.udf.commons.NetUtil;
+import org.apache.commons.io.output.StringBuilderWriter;
+import org.apache.commons.lang.ObjectUtils;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDAFEvaluator;
+import org.apache.hadoop.hive.serde2.io.DoubleWritable;
 import org.apache.hadoop.hive.serde2.objectinspector.*;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.IntObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.StringObjectInspector;
+import org.apache.hadoop.io.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -50,6 +56,12 @@ public class UDAFHttpReqEvaluator extends GenericUDAFEvaluator
 
     private transient ListObjectInspector internalMergeOI;
 
+    private ObjectInspectorConverters.Converter textConverter;
+
+
+    // For PARTIAL1 and PARTIAL2
+//    private Object[] partialResult;
+
 
     enum HttpMethod {
         GET, POST, PUT, DELETE
@@ -67,12 +79,17 @@ public class UDAFHttpReqEvaluator extends GenericUDAFEvaluator
         super.init(m, parameters);
         // init output object inspectors
         // The output of a partial aggregation is a list
+        log.info("#init all params inspectors: {}, param 1: {}", Arrays.toString(parameters), parameters[0].getCategory());
         if (m == Mode.PARTIAL1) {
+            log.info("#init enter Mode: {}", m);
             inputOI = UDFHelper.addCtxToFirstStructInsp(objectInspector, parameters[0]);
+
+            textConverter = ObjectInspectorConverters.getConverter(parameters[1], urlInsp);
+
             return ObjectInspectorFactory.getStandardListObjectInspector(inputOI);
 
         } else {
-            log.info("init Mode: {}, all params inspectors: {}, param 1: {}", m, Arrays.toString(parameters), parameters[0].getCategory());
+            log.info("#init enter Mode: {}", m);
             if (!(parameters[0] instanceof ListObjectInspector)) {
                 //no map aggregation.
                 return ObjectInspectorFactory.getStandardListObjectInspector(inputOI);
@@ -89,7 +106,7 @@ public class UDAFHttpReqEvaluator extends GenericUDAFEvaluator
 
     class HttpReqAggBuffer extends AbstractAggregationBuffer{
 
-        private Collection<Object[]> container;
+        private Collection<Collection<Object>> container;
 
         public HttpReqAggBuffer() {
             container = new LinkedHashSet<>();
@@ -116,8 +133,7 @@ public class UDAFHttpReqEvaluator extends GenericUDAFEvaluator
 
         log.info("iterate 5 parameters: {}", Arrays.toString(parameters));
         // TODO: execute http request
-
-        offerCollection(parameters, httpReqAggBuffer);
+        offerCollection(Arrays.asList(parameters), httpReqAggBuffer);
     }
 
     @Override
@@ -125,8 +141,12 @@ public class UDAFHttpReqEvaluator extends GenericUDAFEvaluator
         log.info("terminatePartial start on machine: {}", NetUtil.getNet().getRunInfo());
         HttpReqAggBuffer httpReqAggBuffer = (HttpReqAggBuffer) agg;
 
-        log.info("terminatePartial parameters: {}", Arrays.toString(httpReqAggBuffer.container.toArray()));
-        List<Object[]> ret = new ArrayList<>(httpReqAggBuffer.container.size());
+        log.info("terminatePartial parameters:\n");
+
+        Arrays.stream(httpReqAggBuffer.container.toArray()).forEach(o ->
+            log.info("terminatePartial collection: {}", Arrays.toString((Object[]) o))
+        );
+        List<Collection<Object>> ret = new ArrayList<>(httpReqAggBuffer.container.size());
         ret.addAll(httpReqAggBuffer.container);
         return ret;
     }
@@ -135,9 +155,9 @@ public class UDAFHttpReqEvaluator extends GenericUDAFEvaluator
     public void merge(AggregationBuffer agg, Object partial) throws HiveException {
         log.info("merge start on machine: {}", NetUtil.getNet().getRunInfo());
         HttpReqAggBuffer httpReqAggBuffer = (HttpReqAggBuffer) agg;
-        List<Object[]> partialResult = (List<Object[]>) internalMergeOI.getList(partial);
+        List<ArrayList<Object>> partialResult = (List<ArrayList<Object>>) internalMergeOI.getList(partial);
         if (partialResult != null) {
-            for(Object[] i : partialResult) {
+            for(ArrayList<Object> i : partialResult) {
                 offerCollection(i, httpReqAggBuffer);
             }
         }
@@ -147,17 +167,30 @@ public class UDAFHttpReqEvaluator extends GenericUDAFEvaluator
     public Object terminate(AggregationBuffer agg) throws HiveException {
         log.info("terminate start on machine: {}", NetUtil.getNet().getRunInfo());
         HttpReqAggBuffer httpReqAggBuffer = (HttpReqAggBuffer) agg;
-        List<Object[]> ret = new ArrayList<>(httpReqAggBuffer.container.size());
+        List<Collection<Object>> ret = new ArrayList<>(httpReqAggBuffer.container.size());
         ret.addAll(httpReqAggBuffer.container);
         return ret;
     }
 
 
-    private void offerCollection(Object[] p, HttpReqAggBuffer httpReqAggBuffer) {
-        log.info("offerCollection: object:" + Arrays.toString(p));
-        // FIXME NullPointerException
-        Object[] pCopy =  ((ArrayList<Object>)ObjectInspectorUtils.copyToStandardObject(p, inputOI)).toArray();
-        log.info("offerCollection: pCopy:" + Arrays.toString(pCopy));
+    private void offerCollection(Collection<Object> p, HttpReqAggBuffer httpReqAggBuffer) {
+//       log.info("offerCollection: object:" + Arrays.toString(p));
+       log.info("offerCollection: object:" + Arrays.toString(p.toArray()));
+        // FIXME ClassCast issue
+        /**
+         * #offerCollection classname: class org.apache.hadoop.io.LongWritable
+         * 20/08/25 12:01:45 INFO UDAFHttpReqEvaluator: #offerCollection classname: class java.lang.String
+         * 20/08/25 12:01:45 INFO UDAFHttpReqEvaluator: #offerCollection classname: class org.apache.hadoop.io.IntWritable
+         * 20/08/25 12:01:45 INFO UDAFHttpReqEvaluator: #offerCollection classname: class java.util.LinkedHashMap
+         * 20/08/25 12:01:45 INFO UDAFHttpReqEvaluator: #offerCollection classname: class java.lang.String
+         */
+
+        ArrayList<Object> pCopy =  (ArrayList<Object>)ObjectInspectorUtils.copyToStandardObject(p, inputOI);
+        log.info("#offerCollection pCopy:" + Arrays.toString(pCopy.toArray()));
+//        Arrays.stream(pCopy).forEach( o -> log.info("#offerCollection classname: {}",o.getClass()));
+        pCopy.stream().forEach( o -> log.info("#offerCollection classname: {}",o.getClass()));
         httpReqAggBuffer.container.add(pCopy);
     }
+
+
 }
