f6ba65222bc565afdab2c96105cdb17741e792fd
John1Tang <1253703152@qq.com>
2021-01-27 19:31:00 +0800

    test

--- hive-udfs/src/main/java/com/thenetcircle/service/data/hive/udf/http/RespHandler.java
+++ hive-udfs/src/main/java/com/thenetcircle/service/data/hive/udf/http/RespHandler.java
@@ -4,6 +4,8 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -11,17 +13,22 @@ import static com.thenetcircle.service.data.hive.udf.http.HttpHelper.headers2Map
 
 final class RespHandler implements ResponseHandler<ThreadLocal<Object[]>> {
 
-    private transient ThreadLocal<Object> ctx = new ThreadLocal<>();;
+    private static Logger log = LoggerFactory.getLogger(RespHandler.class);
 
-    private transient ThreadLocal<Object[]> resp = new ThreadLocal<>();;
+    private transient ThreadLocal<Object> ctx = new ThreadLocal<>();
+
+    private transient ThreadLocal<Object[]> resp = new ThreadLocal<>();
 
     public RespHandler(Object ctx) {
         this.ctx.set(ctx);
     }
 
+
+
     @Override
     public ThreadLocal<Object[]> handleResponse(
             final HttpResponse response) throws ClientProtocolException, IOException {
+        log.info("RespHandler set  ctx:{}", ctx.get());
         resp.set(new Object[]{
                 response.getStatusLine().getStatusCode(),
                 headers2Map(response.getAllHeaders()),
